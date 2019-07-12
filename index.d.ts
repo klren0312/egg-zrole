14144fd4338ee32679a614518cbed63be27a5ff2
ren <kuanglong0312@sina.com>
2019-07-12 10:31:49 +0800

    chore(index.d.ts): add index.d.ts
    #5

---
+++ index.d.ts
@@ -0,0 +1,55 @@
+declare module 'egg' {
+  interface EggAppConfig {
+    zrole: {
+      /**
+       * If you need to use adapter instead of policy file
+       * Make it to true, then you also need to config adapterConfig method
+       */
+      useAdapter?: boolean;
+      /**
+       * If you need Anonymous role
+       * that you can set some permission that all people can 
+       * Make it to true
+       */
+      useAnonymous?: boolean;
+      /**
+       * If you need init policy, before your project start
+       * Make it to true, then you also need to config initPolicy method
+       */
+      usePolicyInit?: boolean;
+      /**
+       * If you need make the response that response someone has no permisson by yourself
+       * Make it to true, then you alse need to config customResponse method
+       */
+      useCustomResponse?: boolean;
+      /**
+       * Casbin model config file path
+       */
+      model: string;
+      /**
+       * Casbin policy config file path
+       */
+      policy?: string;
+      /**
+       * Config adapter instead of policy file
+       */
+      adapterConfig(): object;
+      /**
+       * Use it to setting how to get user role
+       * If not set, it will use default method to get user
+       * @param ctx Egg context
+       */
+      getUser(ctx: object): String;
+      /**
+       * If you need init policy, before your project start
+       * @param ctx Egg context
+       */
+      initPolicy(ctx: object): void;
+      /**
+       * Config it to response someone has no permisson by yourself
+       * @param ctx Egg context
+       */
+      customResponse(ctx: object): void;
+    };
+  }
+}
